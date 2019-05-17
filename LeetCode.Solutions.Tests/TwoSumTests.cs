5c026b419cfb6022830add240ce0717c415ef893
Evgeniy Ladyko <evgeniy.ladyko@gmail.com>
2019-05-17 23:47:22 +0400

    Add project files.

---
+++ LeetCode.Solutions.Tests/TwoSumTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Tests
+{
+    [TestClass]
+    public class TwoSumTests
+    {
+        [DataTestMethod]
+        [DataRow(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        public void CorrectnessTest(int [] nums, int target, int [] expected)
+        {
+            var solution = new TwoSumSolution();
+
+            var result = solution.TwoSum(nums, target);
+
+            CollectionAssert.AreEquivalent(expected, result);
+        }
+    }
+}
---
+++ LeetCode.Solutions/TwoSumSolution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LeetCode.Solutions
+{
+    /// <summary>
+    /// https://leetcode.com/problems/two-sum/
+    /// </summary>
+    public class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var indicies = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int first = nums[i];
+
+                for (int j = i; j < nums.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    var second = nums[j];
+
+                    if (first + second == target)
+                    {
+                        indicies[0] = i;
+                        indicies[1] = j;
+
+                        return indicies;
+                    }
+                }
+            }
+
+            return indicies;
+        }
+    }
+}
