f91ef94cd2cf0737d058940742e651a2ebffac1d
Evgeniy Ladyko <evgeniy.ladyko@gmail.com>
2019-05-19 00:57:07 +0400

    added Reverse Integer solution

---
+++ LeetCode.Solutions.Tests/ReverseIntegerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Tests
+{
+    [TestClass]
+    public class ReverseIntegerTests
+    {
+        [DataTestMethod]
+        [DataRow(123, 321)]
+        [DataRow(-123, -321)]
+        [DataRow(120, 21)]
+        [DataRow(0, 0)]
+        [DataRow(1534236469, 0)]
+        public void SolutionTest(int x, int expected)
+        {
+            var solution = new ReverseInteger();
+
+            var result = solution.Reverse(x);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ LeetCode.Solutions/ReverseInteger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Solutions
+{
+    // https://leetcode.com/problems/reverse-integer/
+    public class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            if (x == 0)
+            {
+                return x;
+            }
+            else if (x == int.MinValue)
+            {
+                return 0;
+            }
+
+            var a = Math.Abs(x)
+                .ToString()
+                .ToCharArray();
+
+            Array.Reverse(a);
+
+            var result = new string(a);
+
+            if (result.StartsWith("0"))
+            {
+                result = result.Substring(1, result.Length - 1);
+            }
+
+            if (x < 0)
+            {
+                result = "-" + result;
+            }
+
+            int.TryParse(result, out var r);
+
+            return r;
+        }
+    }
+}
