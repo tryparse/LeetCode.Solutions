d97fd2e6dd3cc810798ae4ac09ec36e323ca18f3
Evgenii Ladyko <Evgenii_Ladyko@epam.com>
2019-10-01 14:08:26 +0400

    ValidParenthesesProblem

---
+++ LeetCode.Solutions.Tests/ValidParenthesesTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Tests
+{
+    [TestClass]
+    public class ValidParenthesesTests
+    {
+        [DataTestMethod]
+        [DataRow("()", true)]
+        [DataRow("()[]{}", true)]
+        [DataRow("(]", false)]
+        [DataRow("([)]", false)]
+        [DataRow("{[]}", true)]
+        public void ValidParenthesesTest(string s, bool expected)
+        {
+            var solver = new ValidParenthesesProblem();
+
+            var actual = solver.IsValid(s);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ LeetCode.Solutions/ValidParenthesesProblem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Solutions
+{
+    public class ValidParenthesesProblem
+    {
+        private readonly Dictionary<char, char> _pairs = new Dictionary<char, char>()
+        {
+            {'{', '}'},
+            {'(', ')'},
+            {'[', ']'}
+        };
+
+        private readonly Stack<char> _stack = new Stack<char>();
+
+        public bool IsValid(string s)
+        {
+            foreach (var c in s)
+            {
+                if (_pairs.ContainsKey(c))
+                {
+                    _stack.Push(c);
+                }
+                else if (_pairs.ContainsValue(c))
+                {
+                    if (_stack.TryPop(out var top))
+                    {
+                        if (_pairs[top] != c)
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return _stack.Count == 0;
+        }
+    }
+}
