51d16cdda42152293de6e3927c7324f0de9a7557
Evgenii Ladyko <Evgenii_Ladyko@epam.com>
2022-08-22 16:00:13 +0400

    StrStr solution implemented

---
+++ LeetCode.Solutions.Tests/StrStr/StrStrSolutionTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.StrStr.Tests
+{
+    [TestClass()]
+    public class StrStrSolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow("hello", "ll", 2)]
+        [DataRow("aaaaa", "bba", -1)]
+        [DataRow("mississippi", "issipi", -1)]
+        public void StrStrTest1(string haystack, string needle, int expected)
+        {
+            var actual = new StrStrSolution().StrStr(haystack, needle);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ LeetCode.Solutions/StrStr/StrStrSolution.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.StrStr
+{
+    public class StrStrSolution
+    {
+        private const int NOT_FOUND = -1;
+
+        public int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrEmpty(needle))
+            {
+                return 0;
+            }
+
+            if (needle.Length > haystack.Length)
+            {
+                return NOT_FOUND;
+            }
+
+            for (var i = 0; i < haystack.Length; i++)
+            {
+                if (haystack.Length - i < needle.Length)
+                {
+                    return NOT_FOUND;
+                }
+
+                if (haystack[i] != needle[0])
+                {
+                    continue;
+                }
+
+                if (needle.Length == 1)
+                {
+                    return i;
+                }
+
+                var equalityCount = 1;
+
+                for (var j = 1; j < needle.Length; j++)
+                {
+                    var sourceIndex = i + j;
+
+                    if (haystack.Length < sourceIndex
+                        || needle[j] != haystack[sourceIndex])
+                    {
+                        break;
+                    }
+
+                    equalityCount++;
+
+                    if (equalityCount == needle.Length)
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return NOT_FOUND;
+        }
+    }
+}
