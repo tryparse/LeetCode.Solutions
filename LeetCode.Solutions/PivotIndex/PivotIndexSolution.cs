a4277b46d367c0e0fa4050705b782cb00419b27a
Evgenii Ladyko <Evgenii_Ladyko@epam.com>
2022-08-24 19:01:21 +0400

    LeetCode75: Day one Pivot Index implemented

---
+++ LeetCode.Solutions/PivotIndex/PivotIndexSolution.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.PivotIndex
+{
+    public class PivotIndexSolution
+    {
+        // [1,7,3,6,5,6]
+        public int PivotIndex(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            var sum = nums.Skip(1).Sum();
+
+            if (sum == 0)
+            {
+                return 0;
+            }
+
+            var sums = RunningSum(nums);
+            var reverse = RunningSumReverse(nums);
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (sums[i] == reverse[i])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private int[] RunningSum(int[] nums)
+        {
+            // 1,2,3,4
+
+            if (nums.Length == 0)
+            {
+                return Array.Empty<int>();
+            }
+
+            var result = new int[nums.Length];
+
+            result[0] = nums[0];
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                result[i] = result[i - 1] + nums[i];
+            }
+
+            return result;
+        }
+
+        private int[] RunningSumReverse(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return Array.Empty<int>();
+            }
+
+            var result = new int[nums.Length];
+
+            result[^1] = nums[^1];
+
+            for (var i = nums.Length - 2; i >= 0; i--)
+            {
+                result[i] = result[i + 1] + nums[i];
+            }
+
+            return result;
+        }
+    }
+}
