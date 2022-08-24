7b9c70670bb5d9c22db1bd345238785f2c767422
Evgenii Ladyko <Evgenii_Ladyko@epam.com>
2022-08-24 17:47:28 +0400

    LeetCode75: Day one Running Sum implemented

---
+++ LeetCode.Solutions/RunningSumOf1DArray/RunningSumSolution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.RunningSumOf1DArray
+{
+    public class RunningSumSolution
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            // 1,2,3,4
+
+            if (nums.Length == 0)
+            {
+                return Array.Empty<int>();
+            }
+
+            var result = new int[nums.Length];
+
+            result[0] = nums[0];
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                result[i] = result[i-1] + nums[i];
+            }
+
+            return result;
+        }
+    }
+}
