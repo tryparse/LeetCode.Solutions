ff63364db4349ed3078de753208effdd9bf6702c
tryparse <evgeniy.ladyko@gmail.com>
2022-09-02 13:08:49 +0400

    LeetCode75: Day two Isomorphic Strings implemented

---
+++ LeetCode.Solutions.Tests/IsomorphicStrings/IsomorphicStringsSolutionTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.IsomorphicStrings.Tests
+{
+    [TestClass()]
+    public class IsomorphicStringsSolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow("egg", "add", true)]
+        [DataRow("foo", "bar", false)]
+        [DataRow("paper", "title", true)]
+        [DataRow("badc", "baba", false)]
+        public void IsIsomorphicTest(string s, string t, bool expected)
+        {
+            var result = new IsomorphicStringsSolution().IsIsomorphic(s, t);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ LeetCode.Solutions/IsomorphicStrings/IsomorphicStringsSolution.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.IsomorphicStrings
+{
+    /// <summary>
+    /// https://leetcode.com/problems/isomorphic-strings/
+    /// </summary>
+    public class IsomorphicStringsSolution
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            /*
+             * Given two strings s and t, determine if they are isomorphic.
+                Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+                All occurrences of a character must be replaced with another character while preserving the order of characters.
+                No two characters may map to the same character, but a character may map to itself.
+            */
+
+            /*
+             * Constraints:
+
+                1 <= s.length <= 5 * 104
+                t.length == s.length
+                s and t consist of any valid ascii character.
+            */
+
+            /*
+                Input: s = "egg", t = "add"
+                Output: true
+
+                [DataRow("badc", "baba", false)]
+            */
+
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            var replaceDictionary = new Dictionary<char, char>();
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+
+                if (replaceDictionary.ContainsKey(c))
+                {
+                    if (replaceDictionary[c] != t[i])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (replaceDictionary.ContainsValue(t[i]))
+                    {
+                        return false;
+                    }
+
+                    replaceDictionary.Add(c, t[i]);
+                }
+            }
+
+            return true;
+        }
+    }
+}
