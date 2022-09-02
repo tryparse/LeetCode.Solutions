e8af8a96ec79a96b642bfa054ce91ebb362f3959
tryparse <evgeniy.ladyko@gmail.com>
2022-09-02 11:38:56 +0400

    clean usings

--- LeetCode.Solutions.Tests/RunningSumOf1DArray/RunningSumSolutionTests.cs
+++ LeetCode.Solutions.Tests/RunningSumOf1DArray/RunningSumSolutionTests.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using LeetCode.Solutions.RunningSumOf1DArray;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LeetCode.Solutions.RunningSumOf1DArray.Tests
 {
