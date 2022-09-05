56d1f00e5a63e08221efaa342a75d97d83e56356
tryparse <evgeniy.ladyko@gmail.com>
2022-09-05 13:45:19 +0400

    LeetCode75: Backspace string compare implemented

---
+++ LeetCode.Solutions/Solutions/BackspaceStringCompare/BackspaceStringCompareSolution.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Solutions.Solutions.BackspaceStringCompare
+{
+    /// <summary>
+    /// https://leetcode.com/problems/backspace-string-compare/
+    /// </summary>
+    public class BackspaceStringCompareSolution
+    {
+        public bool BackspaceCompare(string s, string t)
+        {
+            var s_stack = Slice(s);
+            var t_stack = Slice(t);
+
+            if (s_stack.Count != t_stack.Count)
+            {
+                return false;
+            }
+
+            while (s_stack.TryPop(out var s_pop))
+            {
+                if (s_pop != t_stack.Pop())
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private Stack<char> Slice(string s)
+        {
+            var result = new Stack<char>();
+
+            foreach (var c in s)
+            {
+                if (c == '#')
+                {
+                    result.TryPop(out _);
+
+                    continue;
+                }
+
+                result.Push(c);
+            }
+
+            return result;
+        }
+    }
+}
