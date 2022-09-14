e5b3bddbe4e340efaf2ec85e1375cd875bb6f789
Evgenii Ladyko <Evgenii_Ladyko@epam.com>
2022-09-14 13:32:35 +0400

    LeetCode75: LongestPalindromeSolution (slow)

---
+++ LeetCode.Solutions.Tests/Solutions/LongestPalindrome/LongestPalindromeSolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Solutions.Solutions.LongestPalindrome.Tests
+{
+    [TestClass()]
+    public class LongestPalindromeSolutionTests
+    {
+        [DataTestMethod()]
+        [DataRow("abccccdd", 7)]
+        [DataRow("aaabbbccccdd", 11)]
+        [DataRow("a", 1)]
+        [DataRow("bb", 2)]
+        [DataRow("ccc", 3)]
+        [DataRow("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth", 983)]
+        public void LongestPalindromeTest(string s, int expected)
+        {
+            var actual = new LongestPalindromeSolution().LongestPalindrome(s);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
