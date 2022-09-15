f8663bbecfd6995f2a2ed996466504c5be21030e
Evgenii Ladyko <Evgenii_Ladyko@epam.com>
2022-09-15 18:01:09 +0400

    LeetCode75: NtreePreorderTraversal implemented

--- DebugSolutionRunner/Program.cs
+++ DebugSolutionRunner/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
-using LeetCode.Solutions.Common.BinaryTree;
-using LeetCode.Solutions.Common.Dijkstra;
-using LeetCode.Solutions.Common.Dijkstra.Generic;
-using LeetCode.Solutions.Common.Dijkstra.HeightGrid;
+using LeetCode.Solutions.Common.NTree;
 using LeetCode.Solutions.Solutions.ConstructStringFromBinaryTree;
 
 namespace DebugSolutionRunner
@@ -14,53 +11,12 @@ namespace DebugSolutionRunner
         {
             //var solution = new ConstructStringFromBinaryTreeSolution();
 
-            var values = new int?[] { 1, 2, 3, null, 4 };
-            var expected = "1(2()(4))(3)";
+            var input = new int?[] { 1, null, 3, 2, 4, null, 5, 6 };
 
-            var root = values.ToTreeNode();
-
-            Console.WriteLine($"Actual={root.ConvertToString()}");
-            Console.WriteLine($"Expected={expected}");
+            var root = input.ToTree();
 
             Console.WriteLine("press any key");
             Console.ReadKey(true);
-        }
-
-        private static void WikiDijkstraExample()
-        {
-            var dijkstra = new DijkstraGraph<int>();
-
-            var n1 = new Node(1);
-            var n2 = new Node(2);
-            var n3 = new Node(3);
-            var n4 = new Node(4);
-            var n5 = new Node(5);
-            var n6 = new Node(6);
-
-            dijkstra.AddNode(n1);
-            dijkstra.AddNode(n2);
-            dijkstra.AddNode(n3);
-            dijkstra.AddNode(n4);
-            dijkstra.AddNode(n5);
-            dijkstra.AddNode(n6);
-
-            dijkstra.AddEdge(new Edge(n1, n2, 7));
-            dijkstra.AddEdge(new Edge(n1, n3, 9));
-            dijkstra.AddEdge(new Edge(n1, n6, 14));
-            dijkstra.AddEdge(new Edge(n2, n3, 10));
-            dijkstra.AddEdge(new Edge(n2, n4, 15));
-            dijkstra.AddEdge(new Edge(n3, n4, 11));
-            dijkstra.AddEdge(new Edge(n3, n6, 2));
-            dijkstra.AddEdge(new Edge(n4, n5, 6));
-            dijkstra.AddEdge(new Edge(n5, n6, 9));
-
-            dijkstra.CalculateDistance(n1);
-
-            foreach (var n in dijkstra.Nodes)
-            {
-                var id = (IEntityWithID)n;
-                Console.WriteLine($"ID={id.ID}, {n.Distance}");
-            }
         }
     }
 }
---
+++ LeetCode.Solutions/Common/NTree/Node.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Solutions.Common.NTree
+{
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    public static class NodeExtensions
+    {
+        /// TODO: Bug is HERE!
+        public static Node ToTree(this int?[] input)
+        {
+            if (input == null
+                || input.Length == 0)
+            {
+                return null;
+            }
+
+            Node root = null;
+            Node currentRoot = null;
+
+            // var input = new int?[] { 1, null, 3, 2, 4, null, 5, 6 };
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input[i].HasValue)
+                {
+                    var node = new Node(input[i].Value);
+
+                    if (currentRoot == null)
+                    {
+                        root ??= node;
+                        currentRoot = node;
+                    }
+                    else
+                    {
+                        currentRoot.children ??= new List<Node>();
+                        currentRoot.children.Add(node);
+                    }
+                }
+                else
+                {
+                    currentRoot = currentRoot?.children?.FirstOrDefault() ?? currentRoot;
+                }
+            }
+
+            return root;
+        }
+
+        public static IList<int> PreorderTraversal(this Node root)
+        {
+            var result = new List<int>();
+
+            if (root == null)
+            {
+                return result;
+            }
+
+            result.Add(root.val);
+
+            if (root?.children == null)
+            {
+                return result;
+            }
+
+            foreach (var child in root.children)
+            {
+                result.AddRange(child.PreorderTraversal());
+            }
+
+            return result;
+        }
+    }
+}
