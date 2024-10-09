bd671c7f21843d87787257ac3670d92b948f917c
Evgenii Ladyko <Evgenii_Ladyko@epam.com>
2024-10-09 19:30:30 +0400

    PlusOne solution implemented

---
+++ LeetCode.Solutions/Solutions/Easy/PlusOne/PlusOneSolution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LeetCode.Solutions.Solutions.Easy.PlusOne
+{
+    public class PlusOneSolution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            var shouldIncrement = true;
+
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (shouldIncrement)
+                {
+                    if (digits[i] == 9)
+                    {
+                        digits[i] = 0;
+                        shouldIncrement = true;
+                    }
+                    else
+                    {
+                        digits[i] = digits[i] + 1;
+                        shouldIncrement = false;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (shouldIncrement)
+            {
+                Array.Resize(ref digits, digits.Length + 1);
+
+                Array.Copy(digits, 0, digits, 1, digits.Length - 1);
+
+                digits[0] = 1;
+            }
+
+            return digits;
+        }
+    }
+}
