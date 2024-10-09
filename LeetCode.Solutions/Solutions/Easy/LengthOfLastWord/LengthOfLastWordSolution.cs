23a28cdb0d8c28e589ca4558d40e3c54af289fb2
Evgenii Ladyko <Evgenii_Ladyko@epam.com>
2024-10-09 20:41:20 +0400

    LengthOfLastWord implemented

---
+++ LeetCode.Solutions/Solutions/Easy/LengthOfLastWord/LengthOfLastWordSolution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Solutions.Solutions.Easy.LengthOfLastWord
+{
+    public class LengthOfLastWordSolution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            var words = s
+                .TrimEnd()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+           
+            return words[^1].Length;
+        }
+    }
+}
