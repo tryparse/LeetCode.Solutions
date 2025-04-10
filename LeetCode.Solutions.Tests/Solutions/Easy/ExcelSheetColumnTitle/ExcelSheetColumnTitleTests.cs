7e555451758ae38870b945f837bd27d0461a4a36
tryparse <evgeniy.ladyko@gmail.com>
2025-04-10 17:06:58 +0400

    ExcelSheetColumnTitle implemented

---
+++ LeetCode.Solutions.Tests/Solutions/Easy/ExcelSheetColumnTitle/ExcelSheetColumnTitleTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Solutions.Solutions.Easy.ExcelSheetColumnTitle;
+using Shouldly;
+using Xunit;
+
+namespace LeetCode.Solutions.Tests.Solutions.Easy.ExcelSheetColumnTitle
+{
+    public class ExcelSheetColumnTitleTests
+    {
+        [Theory]
+        [InlineData(1, "A")]
+        [InlineData(2, "B")]
+        [InlineData(3, "C")]
+        [InlineData(26, "Z")]
+        [InlineData(27, "AA")]
+        [InlineData(28, "AB")]
+        [InlineData(701, "ZY")]
+        [InlineData(52, "AZ")]
+        public void ConvertToTitleTest(int n, string expected)
+        {
+            var act = () => new ExcelSheetColumnTitleSolution().ConvertToTitle(n);
+
+            act.ShouldNotThrow();
+            act().ShouldBe(expected);
+        }
+    }
+}
---
+++ LeetCode.Solutions/Solutions/Easy/ExcelSheetColumnTitle/ExcelSheetColumnTitleSolution.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Solutions.Solutions.Easy.ExcelSheetColumnTitle
+{
+    // https://leetcode.com/problems/excel-sheet-column-title/description/
+    public class ExcelSheetColumnTitleSolution
+    {
+        private const byte A = (byte)'A';
+        private const int LettersNumber = 26;
+
+        public string ConvertToTitle(int columnNumber)
+        {
+            if (columnNumber <= LettersNumber)
+            {
+                return new string(GetChar(columnNumber - 1), 1);
+            }
+
+            var stack = new Stack<char>();
+
+            while (columnNumber > 0)
+            {
+                columnNumber--;
+
+                var remainder = columnNumber % LettersNumber;
+
+                stack.Push(GetChar(remainder));
+
+                columnNumber /= LettersNumber;
+            }
+
+            return new string([.. stack]);
+        }
+
+        private char GetChar(int n) => (char)(A + n);
+    }
+}
+
+// 27 AA
+// 28 AB
+// 26
+// 2 B
