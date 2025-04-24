7e8eeec3f0bb73e7ddd4e446b8e433b924ddb093
tryparse <evgeniy.ladyko@gmail.com>
2025-04-24 14:43:18 +0400

    Happy Number solution implemented

---
+++ LeetCode.Solutions.Tests/Solutions/Easy/HappyNumber/HappyNumberSolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace LeetCode.Solutions.Solutions.Easy.HappyNumber.Tests
+{
+    public class HappyNumberSolutionTests
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void IsHappyTest(int n, bool expected)
+        {
+            var result = new HappyNumberSolution().IsHappy(n);
+
+            result.ShouldBe(expected);
+        }
+
+        public static TheoryData<int, bool> TestData =>
+            new TheoryData<int, bool>
+            {
+                { 19, true },
+                { 2, false }
+            };
+    }
+}
